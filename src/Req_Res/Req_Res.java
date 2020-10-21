f33b2b42d5a624b48fd259541bd303d30dc04b96
Stuti-jain <stutijain898@gmail.com>
2020-10-21 20:41:09 +0530

    initials

---
+++ src/Req_Res/Req_Res.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Req_Res;
+
+
+import User.UserLogin;
+import User.UserSignup;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Folio
+ */
+public class Req_Res{
+    String ip="localhost";
+    int port=8806;
+    Socket socket = new Socket(ip,port);
+    ObjectOutputStream OOS=new ObjectOutputStream(socket.getOutputStream());
+    ObjectInputStream OIS=new ObjectInputStream(socket.getInputStream());
+    //private Object OOS;
+    public Req_Res() throws IOException{
+         System.out.println("Connected!");
+    }
+    public void sendUserSignup(UserSignup user,String s) throws IOException, IOException{
+        OOS.writeUTF("User SignUp");
+        OOS.writeObject(user);
+        s=OIS.readUTF();
+    }
+    public void sendUserLogin(UserLogin user,String s) throws IOException{
+        OOS.writeUTF("User Login");
+        OOS.writeObject(user);
+        s=OIS.readUTF();
+    }
+}
