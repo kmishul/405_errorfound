91c8a490b085f005da29457efd940904fdbf4690
Stuti-jain <stutijain898@gmail.com>
2020-10-26 19:12:43 +0530

    cancel booking

--- src/Req_Res/Req_Res.java
+++ src/Req_Res/Req_Res.java
@@ -132,6 +132,12 @@ public class Req_Res implements Serializable{
        DOS1.writeUTF("View Trains");
        
     }
+     public void cancelbooking(String pnr) throws IOException{
+    
+       DOS1.writeUTF("Cancel Booking");
+        DOS1.writeUTF(pnr);
+       
+    }
     public void passDetails() throws IOException{
     
        DOS1.writeUTF("Pass Details");
